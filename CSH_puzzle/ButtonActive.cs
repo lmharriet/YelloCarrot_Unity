a6cc2a0cd5b2411f2ea2199eaa2eed7dbdaf9427
Peepbo <tngus10778@gmail.com>
2020-12-04 22:02:46 +0900

    puzzle scripts, NumberAnim

---
+++ CSH_puzzle/ButtonActive.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActive : MonoBehaviour
+{
+    public GameObject Floor;
+
+    public float lockTime;
+
+    public float maxTime;
+    float curTime;
+
+    bool isActive = false;  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isActive == false && other.tag == "Player")
+        {
+            isActive = true;
+
+            Floor.GetComponent<GroundMovement>().changeLock(lockTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive)
+        {
+            curTime += Time.deltaTime;
+
+            if (curTime > lockTime) isActive = false;
+        }
+    }
+}
