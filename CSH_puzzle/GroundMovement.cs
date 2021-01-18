8e6f50ff213940ab27820dc314165b7f0994b858
Peepbo <tngus10778@gmail.com>
2021-01-18 20:34:40 +0900

    ㅁㄴㅇㄹ

---
+++ CSH_puzzle/GroundMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    Vector3 defaultPos;
+    public Transform endPos;
+
+    public float speed = 1.5f;
+    public bool moveRight = true;
+
+    public bool isLock = false;
+
+    public GameObject Number;
+
+    public void changeLock(float sec)
+    {
+        if (isLock) return;
+
+        StartCoroutine(waitsec(sec));
+    }
+
+    IEnumerator waitsec(float sec)
+    {
+        Number.SetActive(true);
+        isLock = true;
+        yield return new WaitForSeconds(sec);
+        isLock = false;
+        Number.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultPos = transform.position;
+        Number.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLock) return;
+
+        if(moveRight)
+        {
+            transform.position = Vector3.Lerp(transform.position, endPos.position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, endPos.position) < 0.5f) 
+                moveRight = false;
+        }
+
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, defaultPos, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, defaultPos) < 0.5f)
+                moveRight = true;
+        }
+    }
+}
---
+++ CSH_puzzle/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody rigid;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "MoveGround") transform.parent = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "MoveGround") transform.parent = null;
+    }
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controller();
+
+        Jump();
+    }
+
+    private void Controller()
+    {
+        float h = Input.GetAxis("Horizontal") * speed;
+        float v = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(new Vector3(h * Time.deltaTime, 0, v * Time.deltaTime));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(new Vector3(0, 200f, 0));
+        }
+    }
+}
